d78bc1f2b706c165e5e9f5deae4f15d9b74ccb25
banyh <banyhong@gliacloud.com>
2017-09-22 11:45:40 +0800

    move script

--- extract.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-for lang in en ja zh
-do
-    for file in $lang/*.bz2
-    do
-        python WikiExtractor.py -b 50m --processes=32 $file -o ${file/.bz2/} --lang $lang
-    done
-done
